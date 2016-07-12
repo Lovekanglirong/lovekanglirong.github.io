dddd603ce949bc58d9852bdbf3961ae77cdc920e
Lovekanglirong <859617151@qq.com>
2016-07-12 16:51:09 +0800

    add scope demo

---
+++ argular/script.js
@@ -0,0 +1,10 @@
+function EmployeeController($scope) {
+  $scope.department = 'Engineering';
+  $scope.employee = {
+    name: 'Joe the Manager',
+    reports: [
+      {name: 'John Smith'},
+      {name: 'Mary Run'}
+    ]
+  };
+}
