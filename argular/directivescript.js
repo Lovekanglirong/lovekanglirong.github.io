158694a9b2b906dfa85161a249954694be703393
Lovekanglirong <859617151@qq.com>
2016-07-13 16:02:11 +0800

    基本算是通了

--- argular/directivescript.js
+++ argular/directivescript.js
@@ -12,4 +12,29 @@ angular.module("ezstuff",[])
 	}
     }	
 	
-});
+})
+.controller("MyController", function ($scope) {
+    $scope.name = "dreamapple";
+    $scope.age = 20;
+    $scope.changeAge = function(){
+        $scope.age = 0;
+    }
+})
+    .directive("myDirective", function () {
+    var obj = {
+        restrict: "AE",
+        scope: {
+            name: '@myName',
+            age: '=',
+            changeAge: '&changeMyAge'
+        },
+        replace: true,
+        template: "<div class='my-directive'>" +
+            "My name is:<span ng-bind='name'></span><br/>" +
+            "My age is:<span ng-bind='age'></span><br/>" +
+            "Modify names here:<input type='text' ng-model='name'><br/>" +
+            "<button ng-click='changeAge()'>Modify age:</button>" +
+            " </div>"
+    }
+    return obj;
+    });
