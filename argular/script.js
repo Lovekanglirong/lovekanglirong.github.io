040d87e225381b72e0da20519645ba0ef7a893fd
Lovekanglirong <859617151@qq.com>
2016-07-13 11:18:12 +0800

    ng-init 初始化 与 controller 里面所对应的 scope 的重叠作用

--- argular/script.js
+++ argular/script.js
@@ -14,3 +14,24 @@ function subController($scope) {
 	      name: 'John Smith'
 	};
 }
+
+angular.module("ezstuff",[])
+.directive("ezNamecard",function(){
+	return {
+		restrict:"E",
+		replace:true,
+		template:"<div></div>",
+		link:function(scope,element,attrs){
+			var sb = scope.name;
+			element.append("<div>name:" + sb + "</div>");
+	                //ksdjfkjf	
+			//sdjafdsjf
+		}
+	}
+});
+
+function namecardCtrl($scope){
+//	$scope.name = "kanglirong";
+}
+
+
